15569c748d8eb0d85930ad7dffe90430c5dbcdc5
Tico Palabra <ticopalabra70@gmail.com>
2024-05-31 17:20:48 +0100

    Implemented ImageAnalysis use case

--- app/src/main/java/com/example/cameraxapp21/MainActivity.kt
+++ app/src/main/java/com/example/cameraxapp21/MainActivity.kt
@@ -130,6 +130,14 @@ class MainActivity : AppCompatActivity() {
             // Image Capture
             imageCapture = ImageCapture.Builder().build()
 
+            val imageAnalyzer = ImageAnalysis.Builder()
+               .build()
+               .also {
+                   it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                       Log.d(TAG, "Average luminosity: $luma")
+                   })
+               }
+
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -139,7 +147,7 @@ class MainActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture)
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -199,3 +207,24 @@ class MainActivity : AppCompatActivity() {
 
 }
 
+private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+   private fun ByteBuffer.toByteArray(): ByteArray {
+       rewind()    // Rewind the buffer to zero
+       val data = ByteArray(remaining())
+       get(data)   // Copy the buffer into a byte array
+       return data // Return the byte array
+   }
+
+   override fun analyze(image: ImageProxy) {
+
+       val buffer = image.planes[0].buffer
+       val data = buffer.toByteArray()
+       val pixels = data.map { it.toInt() and 0xFF }
+       val luma = pixels.average()
+
+       listener(luma)
+
+       image.close()
+   }
+}
